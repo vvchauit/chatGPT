7f19b3746e8217aac1cab7011fd1457618fccc9c
VAN CHAU <75282797+vvchauit@users.noreply.github.com>
2023-03-08 15:17:01 +0700

    Add files via upload

---
+++ apichatgpt.js
@@ -0,0 +1,52 @@
+// Constants
+const API_KEY = "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";
+const MODEL_TYPE = "gpt-3.5-turbo"; //chatGPT model
+
+// Creates a custom menu in Google Slides
+function onOpen() {
+    SlidesApp.getUi().createMenu("ChatGPT")
+        .addItem("Generate Prompt", "generatePrompt")
+        .addToUi();
+  }
+    
+    const response = UrlFetchApp.fetch("https://api.openai.com/v1/chat/completions", requestOptions);
+    const responseText = response.getContentText();
+    const json = JSON.parse(responseText);
+    const generatedText = json['choices'][0]['message']['content'];
+    Logger.log(generatedText);
+    slide.insertTextBox(generatedText.toString());
+
+
+  // Generates prompt based on the selected text and adds it to a slide
+  function generatePrompt() {
+    const presentation = SlidesApp.getActivePresentation();
+    const selectedText = presentation.getSelection().getCurrentPage().getShapes()[0].getText().asString();
+    const slide = presentation.getSelection().getCurrentPage();
+    const prompt = "Generate 3 points on " + selectedText;
+    const temperature = 0;
+    const maxTokens = 2060;
+  
+    const requestBody = {
+      model: MODEL_TYPE,
+      messages: [{role: "user", content: prompt}],
+      temperature,
+      max_tokens: maxTokens,
+    };
+  
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + API_KEY,
+      },
+      payload: JSON.stringify(requestBody),
+    };
+  
+  
+    const response = UrlFetchApp.fetch("https://api.openai.com/v1/chat/completions", requestOptions);
+    const responseText = response.getContentText();
+    const json = JSON.parse(responseText);
+    const generatedText = json['choices'][0]['message']['content'];
+    Logger.log(generatedText);
+    slide.insertTextBox(generatedText.toString());
+  }
